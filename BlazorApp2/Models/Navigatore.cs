7efb6d1c032e144c4af0e7e771e14c8b3ed0b70e
Lore <Lore@DESKTOP-J005ERJ>
2024-08-23 20:07:31 +0200

    Aggiungere i file di progetto.

---
+++ BlazorApp2/Models/Navigatore.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp2.Models
+{
+    public class Navigatore
+    {
+        public List<Pagina> paginaList;
+        public Navigatore()
+        {
+            paginaList  = new List<Pagina>();
+        }
+        public void AggiungiCronologia(Pagina pagina)
+        {
+            paginaList.Add(pagina);
+        }
+        public void CancellaCronologia()
+        {
+            paginaList.Clear();
+        }
+    }
+}
---
+++ BlazorApp2/Models/Pagina.cs
@@ -0,0 +1,19 @@
+﻿namespace BlazorApp2.Models
+{
+    public class Pagina
+    {
+        public string indirizzo;
+        public DateTime oraAccesso;
+        public int tempodivisita;
+        public Pagina()
+        {
+            indirizzo = string.Empty;
+            tempodivisita = 0;
+            oraAccesso = DateTime.Now;
+        }
+        public void SalvaTempoVisita(int tvisita)
+        {
+            tempodivisita = tvisita;
+        }
+    }
+}
